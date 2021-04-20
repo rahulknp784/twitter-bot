838d15c7fc93c02c276a5f65e54595d5cefa940b
Rahul Jaiswal <68265225+rahulknp784@users.noreply.github.com>
2021-04-21 01:17:04 +0530

    Update like_bot.py

--- like_bot.py
+++ like_bot.py
@@ -1,8 +1,8 @@
 import tweepy
 import time
 
-auth = tweepy.OAuthHandler('GU91gQbpKANigrmzvNLzu83sU', 'h9rGCUoqMeC6pUVOr1nT9PEwnNN9Kc2GWQZsGsCOj0VR400VKn')
-auth.set_access_token('3285277854-m2ey7AKBNPp7Ri4B8AspaeUxPs6DCKHZJzLWTIE', 'FCHSeGKfO9ROQoGr34ww4CMJITzMTgKgi1K23c7FcULzd')
+auth = tweepy.OAuthHandler('GU91gQbpKANigrmzvNLzu83sU', 'h9rGCUoqMeC6pUVOr1nT9PEwnNN9Kc2GWQZsGsCOj0VR400VK')  #n
+auth.set_access_token('3285277854-m2ey7AKBNPp7Ri4B8AspaeUxPs6DCKHZJzLWTIE', 'FCHSeGKfO9ROQoGr34ww4CMJITzMTgKgi1K23c7FcULz')  #d
 
 api = tweepy.API(auth)
 user = api.me()
